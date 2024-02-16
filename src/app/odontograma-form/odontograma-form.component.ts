8c0b0593e7a0e28a81cd8d349a698691680b795c
FherPie <panaonchoatu@hotmail.com>
2024-02-16 06:19:54 -0500

    cambios odontograma
    cambios odontograma

--- src/app/odontograma-form/odontograma-form.component.ts
+++ src/app/odontograma-form/odontograma-form.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnChanges, OnInit, SimpleChanges, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-odontograma-form',
   templateUrl: './odontograma-form.component.html',
   styleUrls: ['./odontograma-form.component.css']
 })
-export class OdontogramaFormComponent {
+export class OdontogramaFormComponent implements OnInit, AfterViewInit, OnChanges {
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+    throw new Error('Method not implemented.');
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+
+  @ViewChild(TemplateRef, { static: true }) odontoTemplate!: TemplateRef<any> ;
+  
+  @ViewChild(TemplateRef, { static: true, read: ViewContainerRef })
+  odontoContainer!: ViewContainerRef;
+
+
+  ngAfterViewInit() {
+    this.odontoContainer.createEmbeddedView(this.odontoTemplate);
+  }
+
+changeColor(_t13: HTMLElement) {
+throw new Error('Method not implemented.');
+}
 
 
   
